7902c20606ddb679246caea2ff87343be96f4125
Andre Cosby <andrecosby1@gmail.com>
2020-09-20 14:41:20 -0400

    in process

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
+let boxes = document.querySelectorAll(".box")
+let boxes2 = document.querySelectorAll(".box2")
+let calculations = document.querySelector("calculatorin")
+let results =0;
+for (let box of boxes){
+    box.addEventListener("click",function(event){
+        if (box.id === "num-="){
+            Solve(calculations);
+            calculations.textcontent = results;
+            console.log(results);
 
+            //if(box.addeventlistener("click", function(event){
+                    calculations.textContent = box.textContent;
+            }));
+        }
+        }
+    })
+}
